5cf0de3a37f02e6460a4ec1de9b4c0c166d50ee3
Zhou Haiming <e0321120@u.nus.edu>
2019-07-01 16:54:34 +0800

    Add Trie

---
+++ Trie/src/Trie.java
@@ -0,0 +1,58 @@
+import java.util.TreeMap;
+
+public class Trie {
+
+    private class Node {
+        boolean isWord;
+        TreeMap<Character, Node> next;
+
+        public Node(boolean isNode) {
+            this.isWord = isNode;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (cur.isWord != true) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    public boolean contains(String word){
+        Node cur=root;
+        for(int i=0;i<word.length();i++){
+            Character c= word.charAt(i);
+            if(cur.next.get(c)==null)
+                return false;
+            cur=cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+
+}
